9b404d4fb9ff6f685de291bfccf1e84f171280e3
Agustin <agustinferraresi5@gmail.com>
2024-07-10 18:58:24 -0300

    QUISE DECIR QUE LA PAGINA DE MI ARCHIVO ESTA TERMINADA :)

--- JS/PAGES/miArchivo/miArchivo.js
+++ JS/PAGES/miArchivo/miArchivo.js
@@ -20,21 +20,15 @@ datos.forEach(cotizacion => {
 function buscarYEliminar() {
     let valorBorrar = this.closest('button');
     let listItem = this.closest('li');
-    console.log(valorBorrar)
 
     listItem.style.display = 'none';
-
-    console.log("valorBorrar id = ",valorBorrar.id)
-    console.log("valorBorrar name (aca se guarda la fecha) = ",valorBorrar.name)
 
     for (let i = 0; i < ls.length; i++) {
         if (valorBorrar.id == ls[i].id && valorBorrar.name == ls[i].fecha) {
             console.log("estoy en el if del for")
-            
+
             let array = Array.from(ls);
             array.splice(i,1);
-
-            console.log("array",array);
 
             localStorage.setItem("monedas",JSON.stringify(array));
 
@@ -109,45 +103,8 @@ favs.forEach(cotizacion => {
     }
 });
 
-
-function eliminar() {
-    // trae el elemento li mas cercano al botón que fue clickeado
-    
-
-    if (elementoLi) {
-        // cambia el display del li a none
-        //elementoLi.style.display = 'none';
-
-        // Encuentra el índice de la cotización correspondiente en el array favs
-        
-        let idBoton = this.id;
-        let botonFecha = this.fecha
-        console.log(botonFecha)
-
-        let indice = favs.findIndex(cotizacion => cotizacion.id == idBoton && cotizacion.fecha == botonFecha); //siempre devuelve -1
-
-        console.log(indice)
-
-
-        if (indice != -1) {
-            console.log("estoy en el if")
-            // Elimina la cotización del array favs
-            favs.splice(indice, 1);
-
-            // Actualiza el estado y guarda los datos en localStorage
-            datos[indice].estado = false;
-            localStorage.setItem("monedas", JSON.stringify(datos));
-        }
-    } else {
-        console.error("No se encontró el elemento");
-    }
-}
-
 // Asignar la función eliminar a cada botón eliminar
 let botonesEliminar = document.querySelectorAll(".eliminar");
 botonesEliminar.forEach(boton => {
     boton.onclick = buscarYEliminar;
 });
-
-console.log("favs");
-console.log(favs);
