ae48ab053ec01a6c07d145d85dabe8e89cc681b4
Agustin <agustinferraresi5@gmail.com>
2024-07-10 18:54:34 -0300

    INFORMES TERMINADO
    LOS REQUERIMIENTOS OBLIGATORIOS ESTAN TERMINADOS :)

--- JS/PAGES/miArchivo/miArchivo.js
+++ JS/PAGES/miArchivo/miArchivo.js
@@ -5,12 +5,46 @@ let favs = [] //aca se guardaran las cotizaciones que esten marcadas como favori
 let listaMonedas = document.getElementById("lista-monedas");
 let contenidoPrincipal = document.getElementById("contenido-principal");
 
+let ls = JSON.parse(localStorage.getItem("monedas"));
+console.log("ls")
+console.log(ls)
 
 datos.forEach(cotizacion => {
     if (cotizacion.estado) {
         favs.push(cotizacion);
     }
 });
+
+
+
+function buscarYEliminar() {
+    let valorBorrar = this.closest('button');
+    let listItem = this.closest('li');
+    console.log(valorBorrar)
+
+    listItem.style.display = 'none';
+
+    console.log("valorBorrar id = ",valorBorrar.id)
+    console.log("valorBorrar name (aca se guarda la fecha) = ",valorBorrar.name)
+
+    for (let i = 0; i < ls.length; i++) {
+        if (valorBorrar.id == ls[i].id && valorBorrar.name == ls[i].fecha) {
+            console.log("estoy en el if del for")
+            
+            let array = Array.from(ls);
+            array.splice(i,1);
+
+            console.log("array",array);
+
+            localStorage.setItem("monedas",JSON.stringify(array));
+
+        }else if(i == ls.length){
+            alert("Error no hay coincidencias");
+        }
+    }
+}
+
+
 
 //convierto una fecha en formato 'YYYY-MM-DD' a un objeto Date
 function convertirAFecha(fechaString) {
@@ -48,7 +82,7 @@ favs.forEach(cotizacion => {
             `
             <div>
                 <h4>${cotizacion.nombre.slice(0,17)}</h4> <span class="compra">compra $${cotizacion.compra}</span><span class="venta">venta $${cotizacion.venta}</span>
-                <button class="eliminar" id="${cotizacion.id}" fecha="${cotizacion.fecha}"><img src="..//IMG/trash-white.png" alt=""></button>
+                <button class="eliminar" id="${cotizacion.id}" name="${cotizacion.fecha}"><img src="..//IMG/trash-white.png" alt=""></button>
                 <p>${cotizacion.fecha.slice(0,10)}</p>
             </div>
             `
@@ -58,7 +92,7 @@ favs.forEach(cotizacion => {
             `
             <div>
                 <h4>${cotizacion.nombre}</h4> <span class="compra">compra $${cotizacion.compra}</span><span class="venta">venta $${cotizacion.venta}</span>
-                <button class="eliminar" id="${cotizacion.id}" fecha="${cotizacion.fecha}"><img src="..//IMG/trash-white.png" alt=""></button>
+                <button class="eliminar" id="${cotizacion.id}" name="${cotizacion.fecha}"><img src="..//IMG/trash-white.png" alt=""></button>
                 <p>${cotizacion.fecha.slice(0,10)}</p>
             </div>
             `
@@ -78,11 +112,11 @@ favs.forEach(cotizacion => {
 
 function eliminar() {
     // trae el elemento li mas cercano al botón que fue clickeado
-    let elementoLi = this.closest('li');
+    
 
     if (elementoLi) {
         // cambia el display del li a none
-        elementoLi.style.display = 'none';
+        //elementoLi.style.display = 'none';
 
         // Encuentra el índice de la cotización correspondiente en el array favs
         
@@ -112,64 +146,8 @@ function eliminar() {
 // Asignar la función eliminar a cada botón eliminar
 let botonesEliminar = document.querySelectorAll(".eliminar");
 botonesEliminar.forEach(boton => {
-    boton.onclick = eliminar;
+    boton.onclick = buscarYEliminar;
 });
-
-/*
-
-function eliminar() {
-    // Obtener el nombre del botón clicado
-    let nombreBoton = this.id;
-
-    // Encontrar la cotización correspondiente en favs
-    let indice = favs.findIndex(cotizacion => cotizacion.nombre === nombreBoton);
-
-    if (indice !== -1) {
-        // Ocultar el elemento en el DOM
-        let cotizacionBorrar = document.getElementById(nombreBoton);
-        if (cotizacionBorrar) {
-            cotizacionBorrar.style.display = "none";
-        }
-
-        // Eliminar la cotización de favs y actualizar localStorage
-        favs.splice(indice, 1);
-        datos[indice].estado = false;
-        localStorage.setItem("monedas", JSON.stringify(datos));
-    }
-}
-
-// Asignar la función eliminar a cada botón eliminar
-let botonEliminar = document.querySelectorAll(".eliminar");
-botonEliminar.forEach(boton => {
-    boton.onclick = eliminar;
-});
-
-*/
-
-
 
 console.log("favs");
 console.log(favs);
-
-
-/*
-"[{"id":"euro","nombre":"Euro","compra":975.59,"venta":977.03,"estado":false,"fecha":"2024-06-28T20:58:00.000Z","moneda":"EUR"},
-{"id":"real","nombre":"Real Brasileño","compra":162.84,"venta":163.03,"estado":false,"fecha":"2024-06-28T20:58:00.000Z","moneda":"BRL"},
-{"id":"1","nombre":"Blue","compra":1345,"venta":1365,"estado":true,"fecha":"2024-06-28T20:58:00.000Z","moneda":"USD"},
-{"id":"0","nombre":"Oficial","compra":890.5,"venta":930.5,"estado":false,"fecha":"2024-06-28T15:04:00.000Z","moneda":"USD"},
-{"id":"2","nombre":"Bolsa","compra":1338.5,"venta":1348.6,"estado":false,"fecha":"2024-06-28T20:58:00.000Z","moneda":"USD"},
-{"id":"4","nombre":"Mayorista","compra":888.72,"venta":928.64,"estado":false,"fecha":"2024-06-28T20:58:00.000Z","moneda":"USD"},
-{"id":"6","nombre":"Tarjeta","compra":1424.8,"venta":1488.8,"estado":false,"fecha":"2024-06-28T15:04:00.000Z","moneda":"USD"},
-{"id":"3","nombre":"Contado con liquidación","compra":1347,"venta":1349.8,"estado":true,"fecha":"2024-06-28T20:58:00.000Z","moneda":"USD"}
-,{"id":"5","nombre":"Cripto","compra":1346,"venta":1390,"estado":false,"fecha":"2024-06-28T20:58:00.000Z","moneda":"USD"}
-,{"id":"pesoChileno","nombre":"Peso Chileno","compra":96.78,"venta":96.94,"estado":true,"fecha":"2024-06-28T20:58:00.000Z","moneda":"CLP"},
-{"id":"pesoUruguayo","nombre":"Peso Uruguayo","compra":23.09,"venta":23.14,"estado":true,"fecha":"2024-06-28T20:58:00.000Z","moneda":"UYU"}
-,{"id":"euro","nombre":"Euro","compra":975.95,"venta":977.39,"estado":true,"fecha":"2024-06-29T18:00:00.000Z","moneda":"EUR"},
-{"id":"2","nombre":"Bolsa","compra":1338.5,"venta":1348.6,"estado":true,"fecha":"2024-06-30T20:56:00.000Z","moneda":"USD"}
-,{"id":"euro","nombre":"Euro","compra":980.47,"venta":981.91,"estado":true,"fecha":"2024-07-01T20:55:00.000Z","moneda":"EUR"}
-,{"id":"1","nombre":"Blue","compra":1385,"venta":1405,"estado":true,"fecha":"2024-07-01T20:55:00.000Z","moneda":"USD"}]"
-
-
-
-
-*/
